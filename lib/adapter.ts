8c75d635e65a03cf8c14a926457669ada8db59c0
Rishi Raj Jain <rishi@neon.tech>
2024-11-29 15:26:35 +0530

    ws

--- lib/adapter.ts
+++ lib/adapter.ts
@@ -1,7 +1,9 @@
 import { type Adapter } from '@auth/core/adapters'
 import PostgresAdapter from '@auth/pg-adapter'
 import { neonConfig, Pool } from '@neondatabase/serverless'
+import { WebSocket } from 'ws'
 
+neonConfig.webSocketConstructor = WebSocket
 neonConfig.poolQueryViaFetch = true
 
 export const getAdapter = (): Adapter => {
