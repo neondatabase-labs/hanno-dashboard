d1d82aa6efd2edfa952ee8d53d4c61d7c07528e4
Rishi Raj Jain <rishi@neon.tech>
2024-11-29 15:18:59 +0530

    fetch instead of ws

--- lib/adapter.ts
+++ lib/adapter.ts
@@ -1,6 +1,8 @@
 import { type Adapter } from '@auth/core/adapters'
 import PostgresAdapter from '@auth/pg-adapter'
-import { Pool } from '@neondatabase/serverless'
+import { neonConfig, Pool } from '@neondatabase/serverless'
+
+neonConfig.poolQueryViaFetch = true
 
 export const getAdapter = (): Adapter => {
   const pool = new Pool({ connectionString: process.env.DATABASE_URL })
