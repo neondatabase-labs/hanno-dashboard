5e1547fad2578e891fc02bf575bfbed2c0578fb0
Rishi Raj Jain <rishi@neon.tech>
2024-12-18 09:39:57 +0530

    secured

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -15,7 +15,7 @@ export async function POST() {
   if (!session?.user?.email) return new Response(null, { status: 400 })
   const sql = neon(process.env.DATABASE_URL)
   const [{ name, email, image }] = await sql(`SELECT name, email, image FROM users WHERE email = $1 LIMIT 1`, [session.user.email])
-  const salt = 'authjs.session-token'
+  const salt = '__Secure-authjs.session-token'
   const saltVal = await encode({ salt, secret: process.env.AUTH_SECRET, token: { name, email, image } })
   cookieStore.set(salt, saltVal, { secure: true, path: '/', httpOnly: true, sameSite: "lax" })
   return new Response()
