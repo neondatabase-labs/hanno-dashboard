bf9dedd8bafc39bf26b7ec43ac11e7e208754294
Rishi Raj Jain <rishi@neon.tech>
2024-12-18 13:49:56 +0530

    sql

--- lib/providers.ts
+++ lib/providers.ts
@@ -1,4 +1,4 @@
-import { Client, neonConfig } from '@neondatabase/serverless'
+import { neon, neonConfig } from '@neondatabase/serverless'
 import Credentials from 'next-auth/providers/credentials'
 import Google from 'next-auth/providers/google'
 import { comparePassword, generateRandomString, hashPassword } from './credentials'
@@ -15,10 +15,8 @@ export default [
     authorize: async (credentials) => {
       if (!process.env.DATABASE_URL || !credentials.email || typeof credentials.email !== 'string' || !credentials.password || typeof credentials.password !== 'string') return null
       const randomizedPassword = generateRandomString(credentials.password)
-      const client = new Client({ connectionString: process.env.DATABASE_URL })
-      await client.connect()
-      const { rows } = await client.query(`SELECT * FROM users where email = $1 LIMIT 1`, [credentials.email])
-      const userByEmail = rows[0]
+      const sql = neon(process.env.DATABASE_URL)
+      const [userByEmail] = await sql(`SELECT * FROM users where email = $1 LIMIT 1`, [credentials.email])
       if (userByEmail) {
         if (userByEmail.password) {
           const hashedPassword = await hashPassword(randomizedPassword)
@@ -31,13 +29,13 @@ export default [
         }
         throw new Error(`you are using some other authentication method already, but not credentials.`)
       } else {
-        const { rows: userRow } = await client.query(`INSERT INTO users (name, email, image, password) VALUES ($1, $2, $3, $4) RETURNING name, email, image`, [
+        const [userRow] = await sql(`INSERT INTO users (name, email, image, password) VALUES ($1, $2, $3, $4) RETURNING name, email, image`, [
           null,
           credentials.email,
           null,
           randomizedPassword,
         ])
-        return userRow[0]
+        return userRow
       }
     },
   }),
