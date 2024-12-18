8143af2b8f094f47d3528fb99d3adf56d29ee120
Rishi Raj Jain <rishi@neon.tech>
2024-12-18 09:25:29 +0530

    paralleize

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -11,13 +11,12 @@ neonConfig.poolQueryViaFetch = true
 
 export async function POST() {
   if (!process.env.DATABASE_URL || !process.env.AUTH_SECRET) return new Response(null, { status: 500 })
-  const session = await auth()
+  const [session, cookieStore] = await Promise.all([auth(), cookies()])
   if (!session?.user?.email) return new Response(null, { status: 400 })
   const sql = neon(process.env.DATABASE_URL)
   const rows = await sql(`SELECT name, email, image FROM users WHERE email = $1 LIMIT 1`, [session.user.email])
   const { name, email, image } = rows[0]
   const tmp = await encode({ salt: 'authjs.session-token', secret: process.env.AUTH_SECRET, token: { name, email, image } })
-  const cookieStore = await cookies()
   cookieStore.set('authjs.session-token', tmp)
   return new Response()
 }
