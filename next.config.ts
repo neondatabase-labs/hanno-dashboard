39da263e930229331023f30b384f2d63deea192a
Rishi Raj Jain <rishi@neon.tech>
2024-12-19 19:38:33 +0530

    support for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 import type { NextConfig } from 'next'
+
+if (process.env.NODE_ENV === 'development') await setupDevPlatform()
 
 export default {} as NextConfig
