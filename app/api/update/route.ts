1934ed8d882c090d40947f90bd8120016976f669
Rishi Raj Jain <rishi@neon.tech>
2024-12-20 22:22:07 +0530

    remove use of new Response

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -8,22 +8,23 @@ import { auth } from '@/lib/auth'
 import { encode } from '@auth/core/jwt'
 import { neon, neonConfig } from '@neondatabase/serverless'
 import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
 
 neonConfig.poolQueryViaFetch = true
 
 export async function GET(request: Request) {
   const useSecureCookie = request.url.startsWith('https:')
   const salt = useSecureCookie ? '__Secure-authjs.session-token' : 'authjs.session-token'
-  if (!process.env.DATABASE_URL || !process.env.AUTH_SECRET) return new Response(null, { status: 500 })
+  if (!process.env.DATABASE_URL || !process.env.AUTH_SECRET) return new NextResponse(null, { status: 500 })
   const [session, cookieStore] = await Promise.all([auth(), cookies()])
-  if (!session?.user?.email) return new Response(null, { status: 400 })
+  if (!session?.user?.email) return new NextResponse(null, { status: 400 })
   const sql = neon(process.env.DATABASE_URL)
   const [userData] = await sql(`SELECT name, email, image FROM users WHERE email = $1 LIMIT 1`, [session.user.email])
-  if (!userData?.email) cookieStore.set(salt, '', { path: '/', httpOnly: true, sameSite: 'lax', secure: useSecureCookie, maxAge: 0 })
+  if (!userData?.email) cookieStore.set(salt, toString(), { secure: useSecureCookie, path: '/', httpOnly: true, sameSite: 'lax', maxAge: 0 })
   else {
     const { name, email, image } = userData
     const saltVal = await encode({ salt, secret: process.env.AUTH_SECRET, token: { name, email, picture: image } })
     cookieStore.set(salt, saltVal, { secure: useSecureCookie, path: '/', httpOnly: true, sameSite: 'lax' })
   }
-  return new Response()
+  return new NextResponse()
 }
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -13,8 +13,8 @@ neonConfig.poolQueryViaFetch = true
 export async function POST(request: Request) {
   try {
     const session = await auth()
-    if (!process.env.DATABASE_URL) return new Response(null, { status: 500 })
-    if (!session?.user?.email) return new Response(null, { status: 400 })
+    if (!session?.user?.email) return new NextResponse(null, { status: 400 })
+    if (!process.env.DATABASE_URL) return new NextResponse(null, { status: 500 })
     const body = await request.json()
     const sql = neon(process.env.DATABASE_URL)
     if (body.name) await sql(`UPDATE users SET name = $1 WHERE email = $2`, [body.name, session.user.email])
@@ -30,8 +30,8 @@ export async function POST(request: Request) {
 export async function GET(request: Request) {
   try {
     const session = await auth()
-    if (!session?.user?.email) return new Response(null, { status: 400 })
-    if (!process.env.DATABASE_URL) return new Response(null, { status: 500 })
+    if (!session?.user?.email) return new NextResponse(null, { status: 400 })
+    if (!process.env.DATABASE_URL) return new NextResponse(null, { status: 500 })
     const url = new URL(request.url)
     const page = parseInt(url.searchParams.get('page') || '1', 10)
     const limit = parseInt(url.searchParams.get('limit') || '10', 10)
